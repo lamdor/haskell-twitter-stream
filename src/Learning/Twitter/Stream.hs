7ed078e4640f2242597dfc16325d5ad251d93a6a
Luke Amdor <luke.amdor@gmail.com>
2014-08-26 16:53:56 -0500

    using just http-client instead

--- src/Learning/Twitter/Stream.hs
+++ src/Learning/Twitter/Stream.hs
@@ -7,25 +7,24 @@ import Control.Monad.IO.Class
 import Data.Aeson (Value)
 import Data.ByteString
 import Data.Conduit
+import qualified Data.Conduit.List as CL
 import Learning.Twitter.Conduit
 import Learning.Twitter.OAuth
 import Learning.Twitter.URL
-import Network.HTTP.Conduit
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
 
 readTwitterStream :: (MonadResource m) => 
                      URL -> OAuth -> Credential ->
                      m (Source m Value)
 readTwitterStream url oauth creds = do
-  manager   <- liftIO $ newManager conduitManagerSettings
+  manager   <- liftIO $ newManager tlsManagerSettings
   signedReq <- signReq url oauth creds
-  response  <- http signedReq manager
-  let jsonResumableStream = responseBody response $=+ parseToJsonConduit
-  (s,f)     <- unwrapResumable jsonResumableStream
-  return . (addCleanupManager manager) . (addCleanup (const f)) $ s
- where
+  response  <- liftIO $ responseOpen signedReq manager
+  bytes     <- liftIO $ responseBody response
+  let jsonStream = CL.sourceList [bytes] $= parseToJsonConduit
+  return jsonStream
+  where
    signReq url oauth creds = do
      request <- parseUrl url
      signOAuth oauth creds request
-   addCleanupManager manager = addCleanup (const (liftIO $ closeManager manager))
-         
-
