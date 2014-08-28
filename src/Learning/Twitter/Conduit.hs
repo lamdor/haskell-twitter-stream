a4a3b7c098ae12a598b796deb05fdc86e0e3dc88
Luke Amdor <luke.amdor@gmail.com>
2014-08-28 06:35:54 -0500

    adding doctests for Learning.Twitter.Conduit

--- src/Learning/Twitter/Conduit.hs
+++ src/Learning/Twitter/Conduit.hs
@@ -12,7 +12,15 @@ import qualified Data.Conduit.List as CL
 import           Data.Monoid
 import qualified Data.Text as T
 
+-- $setup
+-- >>> :set -XOverloadedStrings
+
 -- Sources
+
+-- | Example
+--
+-- >>> foreverSource (return 1 :: IO Int) $$ CL.take 3
+-- [1,1,1]
 
 foreverSource :: (Functor m, Monad m) => m a -> Source m a
 foreverSource m =
@@ -20,8 +28,18 @@ foreverSource m =
 
 -- Conduits
 
+-- | Example
+--
+-- >>> CL.sourceList ["[1,2,3]"] =$= parseToJSONConduit $$ CL.consume :: IO [Value]
+-- [Array (fromList [Number 1.0,Number 2.0,Number 3.0])]
+
 parseToJSONConduit :: MonadThrow m => Conduit ByteString m Value
 parseToJSONConduit = CA.conduitParser json =$= CL.map snd
+
+-- | Example
+--
+-- >>> CL.sourceList ["[1,2,3]"] =$= parseToJSONConduit =$= fromJSONConduit $$ CL.consume :: IO [[Int]]
+-- [[1,2,3]]
 
 fromJSONConduit :: (Monad m, FromJSON b) => Conduit Value m b
 fromJSONConduit = CL.mapM fromJSONM
@@ -31,18 +49,41 @@ fromJSONConduit = CL.mapM fromJSONM
        Success b -> return b
        Error msg -> fail msg
 
+
+-- | Example
+--
+-- >>> CL.sourceList [1,2,3,2,1] =$= countConduit $$ CL.consume
+-- [1,2,3,4,5]
+
 countConduit :: (Monad m) => Conduit a m Int
 countConduit = scanMappendConduit (const (Sum 1)) =$= CL.map getSum
+
+
+-- | Example
+--
+-- >>> CL.sourceList [1,2,3] =$= scanMappendConduit Sum =$= CL.map getSum $$ CL.consume
+-- [1,3,6]
 
 scanMappendConduit :: (Monad m, Monoid b) => (a -> b) -> Conduit a m b
 scanMappendConduit f = CL.scanl go mempty
   where go a b = let b' = b `mappend` f a in (b', b')
   
 -- Sinks
+
+-- | Example
+--
+-- >>> CL.sourceList [1,2] $$ printSink :: IO ()
+-- 1
+-- 2
     
-printSink :: (Show a, MonadResource m) => Sink a m ()
+printSink :: (Show a, MonadIO m) => Sink a m ()
 printSink = CL.mapM_ (liftIO . print)
 
-putTextSink :: (MonadResource m) => Sink T.Text m ()
+-- | Example
+--
+-- >>> CL.sourceList [T.pack "abc"] $$ putTextSink :: IO ()
+-- abc
+
+putTextSink :: (MonadIO m) => Sink T.Text m ()
 putTextSink = CL.mapM_ (liftIO . putStrLn . T.unpack)
 
